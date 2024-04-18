fd273e8068d63ce1c8880b6ca8b4c4732abe7f45
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2024-04-18 12:45:25 +0200

    insert

--- src/main/java/com/elasticsearch/Elastic/Search/controller/ProductController.java
+++ src/main/java/com/elasticsearch/Elastic/Search/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.elasticsearch.Elastic.Search.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,11 @@ public class ProductController {
 	@GetMapping("/find-all")
 	Iterable<Product> findAll() {
 		return productService.getProducts();
+	}
+	
+	@PostMapping("/insert")
+	public Product insertProduct(@RequestBody Product product) {
+		return productService.insertProduct(product);
 	}
 	
 }
